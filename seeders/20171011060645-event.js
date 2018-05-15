7182a7a74d8bd7dc4897b7359a51952ef6388fe9
Rebecca Tay <rebeccatay92@gmail.com>
2018-05-15 11:56:54 +0800

    fixed events not sorting by startDay

--- seeders/20171011060645-event.js
+++ seeders/20171011060645-event.js
@@ -8,7 +8,8 @@ module.exports = {
     // for each itinerary i
     for (var i = 1; i <= 10; i++) {
       // for each location j
-      for (var j = 1; j <= 10; j++) {
+      // locations 1 - 6 day 2. 7 - 10, 11 day 2
+      for (var j = 1; j <= 6; j++) {
         seedArr.push({
           ItineraryId: i,
           eventType: '{"blocks":[{"key":"dhq60","text":"Sightseeing","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}],"entityMap":{}}',
@@ -26,11 +27,29 @@ module.exports = {
           updatedAt: new Date()
         })
       }
+      for (var k = 7; k <= 10; k++) {
+        seedArr.push({
+          ItineraryId: i,
+          eventType: '{"blocks":[{"key":"dhq60","text":"Sightseeing","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}],"entityMap":{}}',
+          startDay: 2,
+          startTime: 28800 + ((k - 6) * 3600),
+          endTime: 28800 + ((k - 6) * 3600) + 3600,
+          loadSequence: (k - 6),
+          notes: '{"blocks":[{"key":"dhq60","text":"Seeded notes in Draft.js JSON. Blah blah blah.","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}],"entityMap":{}}',
+          cost: '{"blocks":[{"key":"dhq60","text":"30","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}],"entityMap":{}}',
+          currency: '{"blocks":[{"key":"dhq60","text":"SGD","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}],"entityMap":{}}',
+          bookingService: `{"blocks":[{"key":"dhq60","text":"${faker.internet.url()}","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}],"entityMap":{}}`,
+          bookingConfirmation: `{"blocks":[{"key":"dhq60","text":"1234567abcdefg","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}],"entityMap":{}}`,
+          LocationId: k,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        })
+      }
       seedArr.push({
         ItineraryId: i,
-        eventType: '{"blocks":[{"key":"dhq60","text":"Lunch, no Location","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}],"entityMap":{}}',
-        startDay: 1,
-        loadSequence: 11,
+        eventType: '{"blocks":[{"key":"dhq60","text":"Event with NO LOCATION","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}],"entityMap":{}}',
+        startDay: 2,
+        loadSequence: 5,
         createdAt: new Date(),
         updatedAt: new Date()
       })
